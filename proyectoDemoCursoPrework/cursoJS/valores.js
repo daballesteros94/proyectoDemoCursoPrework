5cccea349cb06a05469a35fdaf5b8e4c84290247
daballesteros94 <david.ballesteros94@gmai.com>
2022-06-27 00:00:47 -0500

    sesione valores y variables

---
+++ proyectoDemoCursoPrework/cursoJS/valores.js
@@ -0,0 +1,14 @@
+    40
+
+    "David Ballesteros"
+
+    true
+    false
+
+    null
+    undefined
+
+    [1,2,3]
+
+    { nombre: "David" }
+    
---
+++ proyectoDemoCursoPrework/cursoJS/variables.js
@@ -0,0 +1,10 @@
+var nombre = "David";
+
+// declarar
+var edad;
+// iniciarlizar
+edad = 30;
+
+edad
+
+var elementos = ["computadora" , "Celular" ];
